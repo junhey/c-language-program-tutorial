b9cd2571ee4ace296f5d7f49b7d3c3d8bbf2aa52
junhey <junhey@qq.com>
2017-02-23 22:05:32 +0800

    backup

---
+++ codes/lesson7/pointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//未加入指针
+
+void change(int a,int b){
+	int tmp=a;
+	a=b;
+	b=tmp;
+}
+
+int main() {
+	int a=5;
+	int b=3;
+	change(a,b);
+	printf("num a=%d \nnum b=%d \n",a,b);
+}
+
+//gcc pointer.c -o pointer.out
